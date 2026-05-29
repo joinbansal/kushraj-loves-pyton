2a606867119e26ecfcb0c6ef46cc68ae78ba2ae3
Satvinder <48011908+joinbansal@users.noreply.github.com>
2026-05-28 19:30:24 -0700

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,15 @@
+#right angle
+n = int (input ("Enter no of rows: "))
+def right (n) :
+    for i in range (1, n + 1):
+        print ('*' * i) 
+right (n)
+
+
+#left angle 
+n = int(input("Enter no of row") )
+
+def left(n) :
+    for i in range(n,0, -1):
+        print("*"*I)
+left (n)
