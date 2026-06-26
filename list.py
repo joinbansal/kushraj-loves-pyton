b20b8a9760ec8785fb7f34d917331d0ac6ca43a2
Satvinder <48011908+joinbansal@users.noreply.github.com>
2026-06-25 19:43:24 -0700

    Add files via upload

---
+++ list.py
@@ -0,0 +1,16 @@
+#Creat an empty list
+empty_list = []
+print()
+
+# A list of numbers
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+# Use * operator
+triples = [1, 2, 3]
+
+#reverse the given list
+aList = [100, 400, 300, 200, 500]
+aList = [100, 200, 300, 400, 500]
+aList = aList[::-1]
+print(aList,"/n")
