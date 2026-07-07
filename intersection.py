6c47d55a584ece23121679a64a99588c35e48c2e
Satvinder <48011908+joinbansal@users.noreply.github.com>
2026-07-06 19:23:21 -0700

    Add files via upload

---
+++ intersection.py
@@ -0,0 +1,23 @@
+setx ={"green", "blue"}
+sety = {"blue", "yellow"}
+
+print("Original set elements:")
+print(setx)
+print(setx)
+print("/nlntersection of two said sets:")
+
+setz = setz.intersection(sety)
+print(setz)
+
+
+
+setx = {"green","red","blue","pink"}
+sety = {"blue","yellow","green"}
+
+print("Original set elements:")
+print(setx)
+print(sety)
+print("/nlntersection of two said sets:")
+
+setz = setx.union(sety)
+print(setz)
