3ec3e51a9ecca79464c30c50e0e0729d3318d06c
Satvinder <48011908+joinbansal@users.noreply.github.com>
2026-07-16 19:41:13 -0700

    Add files via upload

--- practice6.py
+++ practice6.py
@@ -1,3 +1,12 @@
 word=input("please enter your word:")
 reverse=word[::-1]
 print(reverse)
+
+
+
+number=int(input("Please enter your number:"))
+if number %2==0:
+    print("this is an even number")
+else:
+    print("this is an odd number")
+
---
+++ practice7.py
@@ -0,0 +1,18 @@
+number=int(input("Please enter your number:"))
+if number > 0:
+    print("its a positive number")
+elif number < 0:
+    print("its a negative number")
+else:
+    print("the number is zero")
+
+
+weather=input("Please enter your weather:")
+if weather=="summer":
+    print("in summer i eat cold food")
+elif weather=="fall":
+    print("in fall i eat warm food")
+elif weather=="winter":
+    print("in winter i eat hot food")
+elif weather=="spring":
+    print("in spring i eat balanced tempurature food")
