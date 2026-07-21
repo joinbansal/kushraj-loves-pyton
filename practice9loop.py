f7f54722c1f5e6290f993fff40f405e48e90e262
Satvinder <48011908+joinbansal@users.noreply.github.com>
2026-07-20 19:24:04 -0700

    Add files via upload

---
+++ practice9loop.py
@@ -0,0 +1,16 @@
+kushraj = "mouse"
+for i in kushraj:
+    print(i)
+
+
+# for loop syntax
+for i in range(101):
+        print(i)
+
+
+n = int(input("Enter the number whose sum you want to find: "))
+sum=15
+
+for i in range(1, n+1):
+      sum = sum+i
+      print("\nSum =", sum)
