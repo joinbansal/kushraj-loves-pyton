03ad98aa22773b164ce2436277beb02140280f6d
Satvinder <48011908+joinbansal@users.noreply.github.com>
2026-07-30 19:38:39 -0700

    Add files via upload

--- function.py
+++ function.py
@@ -1,4 +1,16 @@
-def well_wishes():
- print("hello")
- print("how are you")
-well_wishes()
+def greet_customer():
+    print("Welcome to the Lemonade Stand!")
+    print("Fresh lemonade, made just for you")
+
+greet_customer()
+
+
+price_per_cup = float(input("Enter the price per cup"))
+cups_sold = int(input("Enter the number of cups you sold"))
+
+def calculate_total(price, cups):
+    total = price * cups
+    return total
+A = calculate_total(price_per_cup,cups_sold)
+B = round(A,2)
+print(B)  
