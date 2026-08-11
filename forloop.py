c01e776ee563de8f3ade776fc15ffdbd085df768
Satvinder <48011908+joinbansal@users.noreply.github.com>
2026-08-10 19:19:51 -0700

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,34 @@
+
+A = int(input("Please enter you number"))
+for i in range(A, 0 - 1):
+    print(i)
+
+string = input("Please enter you string")
+string2 = ''
+for i in string:
+    string2 = i + string2
+print(string2)
+
+
+string = input("Please enter your string")
+string2 = ''
+for i in string:
+    string2 = i + string2
+print(string2)
+
+
+for i in range(30, 51):
+    print(i)
+
+num = int(input("Please enter you number"))
+sum = 0
+for i in range (1,num + 1):
+    sum = sum + i
+    print(sum)
+
+
+num = int(input("Please enter you number"))
+sum = 0
+for i in range (1, sum + 1):
+    sum = sum + i
+    print(sum)
